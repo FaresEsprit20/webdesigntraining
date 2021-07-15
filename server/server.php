b8c8e752b77087490d070f170cc12247f3a68944
FaresEsprit20 <63284587+FaresEsprit20@users.noreply.github.com>
2021-07-15 12:50:58 +0100

    first commit

---
+++ server/server.php
@@ -0,0 +1,17 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ceci est une page de test avec des balises PHP</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h2>Page de test</h2>
+
+        <?php
+      echo 'hello ragazza';
+      
+        ?>
+    </body>
+</html>
